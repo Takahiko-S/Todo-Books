0adb8f6d301c1483eaece35ee4cb45e8f104ad55
zabieru2308 <55108909+zabieru2308@users.noreply.github.com>
2023-06-07 12:33:31 +0900

    前回のコミットしなおし

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -10,22 +10,23 @@
   <div class="container mt-5">
     <h1 class="text-center mb-5">My 読書リスト</h1>
     <div class="row">
-      <!-- 各本の情報はここでループさせる -->
+        @foreach ($books as $book)
       <div class="col-md-4 mb-4">
         <div class="card">
           <img src="本の画像URL" class="card-img-top" alt="Book Image">
           <div class="card-body">
-            <h5 class="card-title">本の名前</h5>
-            <p class="card-text">作者名</p>
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="readCheck" checked>
-              <label class="form-check-label" for="readCheck">読書済み</label>
-            </div>
-            <a href="詳細ページのURL" class="btn btn-primary mt-3">詳細を見る</a>
+            <h5 class="card-title">{{ $book->title}}</h5>
+            <p class="card-text">{{$book->sakusya}}</p>
+           @if($book->readend && $book->readend>now())
+           <p>日付が間違っています</p>
+           @else
+           <p>読書済み</p>
+           @endif
+            <a href="/books{{ $book->id }}reviews" class="btn btn-primary mt-3">詳細を見る</a>
           </div>
         </div>
       </div>
-      <!-- ループ終了 -->
+      @endforeach
     </div>
   </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,25 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" >
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Bootstrap CSS -->
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('jquery-ui-1.13.2/jquery-ui.min.css')}}" rel="stylesheet">
+        <link href="{{asset('css/lightbox.min.css')}}" rel="stylesheet">
+        <link href="{{asset('css/custom.css')}}" rel="stylesheet">
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="{{asset('js/jquery-3.6.3.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('jquery-ui-1.13.2/jquery-ui.min.js')}}"></script>
+        <script src="{{asset('js/lightbox.min.js')}}"></script>
     </head>
     <body class="font-sans antialiased">
+
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -31,6 +44,9 @@
             <main>
                 {{ $slot }}
             </main>
+
+
+
         </div>
     </body>
 </html>
